ca6c07fd7a58b62159d19fbe9bdc5945fe965bc9
Federico Tomassetti <federico@strumenta.com>
2023-07-20 11:19:58 +0200

    implementing initial interpreter

--- pricing-engine/src/main/kotlin/com/strumenta/pricing/PricingInterpreter.kt
+++ pricing-engine/src/main/kotlin/com/strumenta/pricing/PricingInterpreter.kt
@@ -1,14 +1,34 @@
 package com.strumenta.pricing
 
+import java.math.BigDecimal
+
 data class OrderLine(val productID: String, val quantity: Int)
 data class Order(val lines: List<OrderLine>)
 
-data class Discount(val description: String, val amount: Int)
-data class Price(val basePrice: Int, val currency: Currency, val discounts: List<Discount>)
+data class Discount(val description: String, val amount: PriceComponent)
+data class Price(val basePrice: MutableList<PriceComponent> = mutableListOf(), val discounts: MutableList<Discount> = mutableListOf()) {
+    fun addBasePrice(priceComponent: PriceComponent) {
+        TODO()
+    }
+}
+data class PriceComponent(val value: BigDecimal, val currency: Currency) {
+    fun multipliedBy(multiplier: Int): PriceComponent {
+        return PriceComponent(value.multiply(BigDecimal(multiplier)), currency)
+    }
+}
 
 class PricingInterpreter(val pricingStrategy: PricingStrategy) {
 
+    private fun basePriceFor(productID: String) : PriceComponent {
+        val basePrice = pricingStrategy.basePrices.find { it.itemID == productID } ?: throw IllegalArgumentException("No price for $productID")
+        return PriceComponent(BigDecimal("${basePrice.amount.integerPart}.${basePrice.amount.decimalPart}"), basePrice.amount.currencey)
+    }
+
     fun calculatePrice(order: Order) : Price {
-        TODO()
+        val price = Price()
+        order.lines.forEach { orderLine ->
+            price.addBasePrice(basePriceFor(orderLine.productID).multipliedBy(orderLine.quantity))
+        }
+        return price
     }
 }
--- pricing-engine/src/test/kotlin/PricingInterpreterTest.kt
+++ pricing-engine/src/test/kotlin/PricingInterpreterTest.kt
@@ -1,17 +1,47 @@
 import com.strumenta.kolasu.lionweb.StarLasuLWLanguage
+import com.strumenta.kolasu.model.ReferenceByName
+import com.strumenta.pricing.Amount
+import com.strumenta.pricing.BasePrice
+import com.strumenta.pricing.Currency
+import com.strumenta.pricing.Order
+import com.strumenta.pricing.OrderLine
+import com.strumenta.pricing.Price
+import com.strumenta.pricing.PriceComponent
+import com.strumenta.pricing.PricingInterpreter
+import com.strumenta.pricing.PricingStrategy
 import io.lionweb.lioncore.java.language.Language
 import io.lionweb.lioncore.java.serialization.JsonSerialization
 import java.io.File
 import java.io.FileInputStream
+import java.math.BigDecimal
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class PricingInterpreterTest {
 
     @Test
-    fun calculatePriceSimpleCase() {
+    fun calculatePriceForMPSExportedPricing() {
         val pricingFile = this.javaClass.classLoader.getResourceAsStream("strategies.json")
         require(pricingFile != null)
         val jsonser = JsonSerialization.getStandardSerialization()
         val nodes = jsonser.unserializeToNodes(pricingFile)
+    }
+
+    @Test
+    fun calculatePriceForPricingWithNoDiscounts() {
+        val pricing = PricingStrategy(name = "Standard Prices",
+            sameBasePricesAs = ReferenceByName("", null),
+            basePrices = mutableListOf(
+                BasePrice("xyz", Amount(Currency.EUR, 10, 0))
+            ),
+            discountPolicies = mutableListOf()
+        )
+        val order = Order(
+            lines = listOf(
+                OrderLine("xyz", 7)
+            )
+        )
+        assertEquals(Price(basePrice = mutableListOf(PriceComponent(BigDecimal(70.0), Currency.EUR))),
+            PricingInterpreter(pricing).calculatePrice(order))
     }
 }
