722f58e3b4fe9aa8080f6d4f6107509804e70c75
Federico Tomassetti <federico@strumenta.com>
2023-08-04 14:56:03 +0200

    working on tests

--- pricing-engine/README.md
+++ pricing-engine/README.md
@@ -9,3 +9,6 @@ you can also run `./gradlew :pricing-engine:compileKotlin` from the root project
 
 The project then defines a very simple interpreter (called `PricingInterpreter`). Given an order and a pricing strategy,
 it calculates the price listing the components that lead to that particular price.
+
+At this time both the languages and the models are exported manually from MPS (by a user triggering some intentions)
+and then copied in this project under `src/main/lionweb/pricing.json` (languages) and `src/test/resources/strategies.json` (models).
---
+++ pricing-engine/src/main/kotlin/com/strumenta/pricing/PricingLoader.kt
@@ -0,0 +1,42 @@
+package com.strumenta.pricing
+
+import com.strumenta.kolasu.language.KolasuLanguage
+import com.strumenta.kolasu.lionweb.LionWebModelConverter
+import com.strumenta.kolasu.lionweb.StarLasuLWLanguage
+import io.lionweb.lioncore.java.language.Language
+import io.lionweb.lioncore.java.serialization.JsonSerialization
+import java.io.File
+import java.io.FileInputStream
+
+class PricingLoader {
+    private var language: Language
+    private var jsonser: JsonSerialization
+
+    init {
+        val languageFile = File("src/main/lionweb/pricing.json")
+        require(languageFile.exists())
+        jsonser = JsonSerialization.getStandardSerialization()
+        jsonser.nodeResolver.addTree(StarLasuLWLanguage)
+        language = jsonser.unserializeToNodes(FileInputStream(languageFile)).first() as Language
+        jsonser.conceptResolver.registerLanguage(language)
+    }
+
+    fun loadPricingStrategyFromLionWebFile(lionwebFile: File): PricingStrategy {
+        require(lionwebFile.exists())
+        jsonser.enableDynamicNodes()
+        val model = jsonser.unserializeToNodes(FileInputStream(lionwebFile))
+        println(model)
+
+        val lwImpExp = LionWebModelConverter()
+        val kolasuLanguage = KolasuLanguage("pricing").apply {
+            addClass(Amount::class)
+            addEnumClass(Currency::class)
+            addClass(PricingStrategy::class)
+            addClass(IntLiteral::class)
+        }
+
+        lwImpExp.associateLanguages(language, kolasuLanguage)
+        lwImpExp.importModelFromLionWeb(model.first())
+        return model.first() as PricingStrategy
+    }
+}
